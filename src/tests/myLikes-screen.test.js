e5b80365b08354c3ded1666ea612094692e7cc46
Maulik Joshi <maulikjoshi@Mauliks-MacBook-Pro.local>
2022-04-02 20:55:59 -0400

    Added test cases

---
+++ src/tests/myLikes-screen.test.js
@@ -0,0 +1,77 @@
+import Tuits from "../components/tuits";
+import {screen, render} from "@testing-library/react";
+import {HashRouter} from "react-router-dom";
+import {findAllTuitsLikedByUser} from "../services/likes-service";
+import axios from "axios";
+
+
+const mock = jest.spyOn(axios, 'get');
+
+const MOCKED_LIKED_TUITS = [
+
+    {
+        _id: "12345",
+        tuit: "Random tuit 1",
+        postedBy: "ABC",
+        postedOn: "2022-02-26T00:00:00.000+00:00",
+        stats: {
+            dislikes: 1,
+            likes: 1,
+            replies: 0,
+            retuits: 0
+        }
+    }
+
+    ,
+
+    {
+        _id: "98765",
+        tuit: "Random tuit 2",
+        postedBy: "XYZ",
+        postedOn: "2022-02-26T00:00:00.000+00:00",
+        stats: {
+            dislikes: 1,
+            likes: 1,
+            replies: 0,
+            retuits: 0
+        }
+    }
+];
+
+
+test('tuit list renders static liked tuits array', () => {
+    render(
+        <HashRouter>
+            <Tuits tuits={MOCKED_LIKED_TUITS}/>
+        </HashRouter>
+    );
+    const linkElement = screen.getByText(/Random tuit 1/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('list of liked tuit renders mocked', async () => {
+    axios.get.mockImplementation(() =>
+        Promise.resolve({data: {tuits: MOCKED_LIKED_TUITS}}));
+    const res = await findAllTuitsLikedByUser("XYZ");
+    const tuit2 = res.tuits;
+
+    render(
+        <HashRouter>
+            <Tuits tuits={tuit2}/>
+        </HashRouter>);
+
+    const tuit = screen.getByText(/Random tuit 2/i);
+    expect(tuit).toBeInTheDocument();
+});
+
+
+test('tuits lists that are rendered on my screen', async () => {
+    mock.mockRestore();
+    const tuits = await findAllTuitsLikedByUser("ABC")
+    render(
+        <HashRouter>
+            <Tuits tuits={tuits}/>
+        </HashRouter>);
+    const linkElement = screen.getByText(/Random tuit 1/i);
+    expect(linkElement).toBeInTheDocument();
+})
--- src/tests/testing-mylikes.test.js
+++ src/tests/testing-mylikes.test.js
@@ -1,7 +1,7 @@
 import {act, create} from 'react-test-renderer';
 import TuitStats from "../components/tuits/tuit-stats";
 
-test('stats render correctly', () => {
+test('Adding stats render correctly', () => {
     let stats = {
         likes: 123,
         dislikes: 10,
@@ -21,13 +21,53 @@ test('stats render correctly', () => {
         })
     }
 
-    const dislikeTuit = () => {
+    let tuitStats
+    act(() => {
+        tuitStats = create(
+            <TuitStats
+                likeTuit={likeTuit}
+                tuit={{stats: stats}}/>
+        );
+    })
+
+    const root = tuitStats.root;
+    const likesCounter = root.findByProps({className: 'ttr-stats-likes'})
+    const retuitsCounter = root.findByProps({className: 'ttr-stats-retuits'})
+    const repliesCounter = root.findByProps({className: 'ttr-stats-replies'})
+    const likeTuitButton = root.findByProps(
+        {className: 'ttr-like-tuit-click'})
+
+    let likesText = likesCounter.children[0];
+    const repliesText = repliesCounter.children[0];
+    const retuitsText = retuitsCounter.children[0];
+    expect(likesText).toBe('123');
+    expect(repliesText).toBe('234');
+    expect(retuitsText).toBe('345');
+
+    act(() => {
+        likeTuitButton.props.onClick()
+    })
+
+    likesText = likesCounter.children[0];
+    expect(likesText).toBe('124');
+});
+
+
+test('Removing stats render correctly', () => {
+    let stats = {
+        likes: 123,
+        dislikes: 10,
+        replies: 234,
+        retuits: 345
+    }
+
+    const likeTuit = () => {
         act(() => {
-            stats.dislikes++;
+            stats.likes--;
             tuitStats.update(
                 <TuitStats
                     tuit={{stats: stats}}
-                    dislikeTuit={() => {
+                    likeTuit={() => {
                     }}
                 />)
         })
@@ -38,40 +78,28 @@ test('stats render correctly', () => {
         tuitStats = create(
             <TuitStats
                 likeTuit={likeTuit}
-                dislikeTuit={dislikeTuit}
                 tuit={{stats: stats}}/>
         );
     })
 
     const root = tuitStats.root;
     const likesCounter = root.findByProps({className: 'ttr-stats-likes'})
-    const dislikesCounter = root.findByProps({className: 'ttr-stats-dislikes'})
     const retuitsCounter = root.findByProps({className: 'ttr-stats-retuits'})
     const repliesCounter = root.findByProps({className: 'ttr-stats-replies'})
     const likeTuitButton = root.findByProps(
         {className: 'ttr-like-tuit-click'})
-    const dislikeTuitButton = root.findByProps(
-        {className: 'ttr-dislike-tuit-click'})
 
     let likesText = likesCounter.children[0];
-    let dislikesText = dislikesCounter.children[0];
     const repliesText = repliesCounter.children[0];
     const retuitsText = retuitsCounter.children[0];
     expect(likesText).toBe('123');
     expect(repliesText).toBe('234');
     expect(retuitsText).toBe('345');
-    expect(dislikesText).toBe('10');
 
     act(() => {
         likeTuitButton.props.onClick()
     })
 
-    act(() => {
-        dislikeTuitButton.props.onClick()
-    })
     likesText = likesCounter.children[0];
-    expect(likesText).toBe('124');
-
-    dislikesText = dislikesCounter.children[0];
-    expect(dislikesText).toBe('10');
+    expect(likesText).toBe('122');
 });
